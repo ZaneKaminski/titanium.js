d8f983721edffd4aefebb6097300da526dda4874
Zane Kaminski <zanekaminski@gmail.com>
2015-06-23 02:50:22 -0400

    Initial commit.

---
+++ titanium.js
@@ -0,0 +1,23 @@
+/// <reference path="typings/jquery/jquery.d.ts"/>
+(function($) {
+    $.fn.titanium = function(action) {
+        var navbar = this;
+        
+        if (action == 'refresh') {
+            var titlebar = navbar.before();
+            if (this.before().inView('any') === true) {
+                titlebar.css({ paddingBottom: 0 });
+                navbar.addClass('navbar-static-top').removeClass('navbar-fixed-top');
+            } else {
+                titlebar.css({ paddingBottom: navbar.outerHeight(true) });
+                navbar.removeClass('navbar-static-top').addClass('navbar-fixed-top');
+            }
+        }
+        else { // action == 'install'
+            navbar.titanium('refresh');
+            $(window).scroll(function() {
+                navbar.titanium('refresh');
+            });
+        }
+    };
+ }(jQuery));
