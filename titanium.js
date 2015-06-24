37f2fba57f019edaf6052d8eae79d05a54392007
Zane Kaminski <zanekaminski@gmail.com>
2015-06-23 23:16:48 -0400

    Small change to formatting of titanium.js.

--- titanium.js
+++ titanium.js
@@ -1,6 +1,6 @@
 /// <reference path="typings/jquery/jquery.d.ts"/>
 (function($) {
-    $.fn.titanium = function(action) {
+    $.fn.titanium = function(action, options) {
         var navbar = this;
         
         if (action == 'refresh') {
@@ -12,8 +12,7 @@
                 titlebar.css({ marginBottom: navbar.outerHeight(true) });
                 navbar.removeClass('navbar-static-top').addClass('navbar-fixed-top');
             }
-        }
-        else { // action == 'install'
+        } else { // action == 'install'
             navbar.titanium('refresh');
             $(window).scroll(function() {
                 navbar.titanium('refresh');
